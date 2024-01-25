43dcf7c151a02efc68b5f05c8e1f9bee32b46068
k-mencser-b <mencser.b@gmail.com>
2024-01-25 13:53:08 +0100

    minden vissza

--- src/pages/DrBabor1.jsx
+++ src/pages/DrBabor1.jsx
@@ -1,9 +1,13 @@
 import classes from './DrBaborPro.module.css';
+import babor1image from '../Assets/babor1.jpg';
 
 const DrBabor1 = () => {
     return(
         <>
-        <h1 className={classes.baborpro}>Dr babor 1</h1>
+        <h1 className={classes.baborpro}>Dr Babor Kezelés</h1>
+        <div className={classes.babor1}>
+          <img src={babor1image} alt='babor1image'></img>
+        </div>
         <p>Valamin szöveg</p>
         </>
     )
